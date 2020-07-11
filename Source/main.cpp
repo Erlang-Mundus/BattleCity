f9befd668bb4ad56de6961b5f377de7015670201
Erlan <bmundusov@gmail.com>
2020-07-11 14:34:22 +0300

    Draw first trianfle

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,6 +3,36 @@
 
 #include <iostream>
 
+GLfloat Points[] = {
+	0.0f,0.5f,0.0f,
+	0.5f,-0.5f,0.0f,
+	-0.5f,-0.5f,0.0f
+};
+
+GLfloat Colors[] = {
+	1.0f,0.0f,0.0f,
+	0.0f,1.0f,0.0f,
+	0.0f,0.0f,1.0f,
+};
+
+const char* vertex_shader =
+"#version 460\n"
+"layout(location = 0) in vec3 vertex_position;"
+"layout(location = 1) in vec3 vertex_color;"
+"out vec3 color;"
+"void main(){"
+"	color = vertex_color;"
+"	gl_Position = vec4(vertex_position, 1.0);"
+"}";
+
+const char* fragment_shader =
+"#version 460\n"
+"in vec3 color;"
+"out vec4 frag_color;"
+"void main(){"
+"	frag_color = vec4 (color, 1.0);"
+"}";
+
 int WindowSizeX = 640;
 int WindowSizeY = 480;
 
@@ -59,11 +89,54 @@ int main(void)
 	
 	glClearColor(1,1,0,1);
 	
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertex_shader,nullptr);
+	glCompileShader(vs);
+
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragment_shader, nullptr);
+	glCompileShader(fs);
+
+	GLuint ShaderProgram = glCreateProgram();
+	glAttachShader(ShaderProgram, vs);
+	glAttachShader(ShaderProgram, fs);
+	glLinkProgram(ShaderProgram);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	// Generate buffers
+	GLuint PointsVBO = 0;
+	glGenBuffers(1, &PointsVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, PointsVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Points), Points, GL_STATIC_DRAW);
+
+	GLuint ColorsVBO = 0;
+	glGenBuffers(1, &ColorsVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, ColorsVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), Colors, GL_STATIC_DRAW);
+
+	GLuint VAO = 0;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, PointsVBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, ColorsVBO);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(ShaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
